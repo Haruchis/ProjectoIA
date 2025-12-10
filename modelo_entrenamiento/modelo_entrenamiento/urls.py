073ca0a1b0d9c393e401aa1c61ed67252242e052
Carlos Perdomo <cpcarlosperdomo@hotmail.com>
2025-12-10 10:22:32 -0500

    Arreglo urls.py y config de rutas

--- modelo_entrenamiento/modelo_entrenamiento/urls.py
+++ modelo_entrenamiento/modelo_entrenamiento/urls.py
@@ -1,5 +1,4 @@
 """modelo_entrenamiento URL Configuration."""
-codex/create-django-project-for-random-forest-model-qz73b2
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -8,18 +7,12 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Cuando entren a la raíz "/", redirige a la vista de entrenamiento
     path('', RedirectView.as_view(pattern_name='train_model', permanent=False)),
+    # URLs de la app "prediccion" (train/, predict/, etc.)
     path('', include('prediccion.urls')),
 ]
 
+# Para servir archivos subidos (CSV, etc.) en modo DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('prediccion.urls')),
-]
-main
